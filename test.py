247b2860aa53cc750c6395c581e9920e1eefac28
Adrian <adriansandovalvargas@gmail.com>
2019-07-05 13:54:59 -0400

    Play/Pause
    Implemented Play/Pause commands

--- test.py
+++ test.py
@@ -12,7 +12,10 @@ def MediaPlayer():
     else:
         if "prev_btn" in request.form:
             print("Prev Clicked")
-            os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Previous")
+            try:
+                os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Previous")
+            except Exception as e:
+                print(str(e))
             if click == 1:
                 return render_template("pause.html")
             if click == 0:
@@ -23,15 +26,26 @@ def MediaPlayer():
             if click == 0:
                 click += 1
                 print(click)
+                try:
+                    os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Play")
+                except Exception as e:
+                    print(str(e))
                 return render_template("pause.html")
             if click == 1:
                 click = 0
                 print(click)
+                try:
+                    os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Pause")
+                except Exception as e:
+                    print(str(e))
                 return render_template("play.html")
 
         if "next_btn" in request.form:
             print("Next Clicked")
-            os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Next")
+            try:
+                os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Next")
+            except Exception as e:
+                print(str(e))
             if click == 1:
                 return render_template("pause.html")
             if click == 0:
