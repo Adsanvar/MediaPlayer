4ccf57a44479e1ea395cc3d95132daeb6b11d6b2
Adrian <adriansandovalvargas@gmail.com>
2019-07-05 11:35:23 -0400

    Layout
    Finalized layout for Raspberry Pi 7' display.
    Added Post methods captures for buttons.

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-    return render_template('main.html')
+@app.route('/', methods=['GET', 'POST'])
+def MediaPlayer():
+    if "prev_btn" in request.form:
+        print("Prev Clicked")
+    if "play_btn" in request.form:
+        print("Play Clicked")
+    if "next_btn" in request.form:
+        print("Next Clicked")
 
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug = True, port=8080)
