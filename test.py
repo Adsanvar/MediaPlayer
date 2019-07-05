8b9eac920b8d15f47a942770033f6b367664d7ba
Adrian <adriansandovalvargas@gmail.com>
2019-07-05 13:41:53 -0400

    Bluetooh Commands
    Implemented Previous/Next commands

--- test.py
+++ test.py
@@ -1,16 +1,42 @@
+import os
 from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 
+click = 0
+
 @app.route('/', methods=['GET', 'POST'])
 def MediaPlayer():
-    if "prev_btn" in request.form:
-        print("Prev Clicked")
-    if "play_btn" in request.form:
-        print("Play Clicked")
-    if "next_btn" in request.form:
-        print("Next Clicked")
+    global click
+    if request.method == 'GET':
+        return render_template("play.html")
+    else:
+        if "prev_btn" in request.form:
+            print("Prev Clicked")
+            os.system("sudo dbus-send --system --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Previous")
+            if click == 1:
+                return render_template("pause.html")
+            if click == 0:
+                return render_template("play.html")
+        if "play_btn" in request.form:
+            print("Play Clicked")
+          
+            if click == 0:
+                click += 1
+                print(click)
+                return render_template("pause.html")
+            if click == 1:
+                click = 0
+                print(click)
+                return render_template("play.html")
 
-    return render_template('main.html')
+        if "next_btn" in request.form:
+            print("Next Clicked")
+            os.system("sudo dbus-send --system --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Next")
+            if click == 1:
+                return render_template("pause.html")
+            if click == 0:
+                return render_template("play.html")
+
 
 if __name__ == '__main__':
     app.run(debug = True, port=8080)
