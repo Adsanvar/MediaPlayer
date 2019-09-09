53ab262fa80867a92f9352da38b2a1811ca4725b
Adrian <adriansandovalvargas@gmail.com>
2019-09-09 19:12:57 -0400

    Up/Down functionality

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -3,12 +3,17 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 #sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E/player2 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track
 click = 0
-
+opener = 0
 def openWeb():
-    port = 8080
-    url = "http://127.0.0.1:"+str(port)
-    print("~~~~~~~~~~~~~~~~~~~~~~~Openning~~~~~~~~~~~~~~~~~")
-    threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+    global opener
+    opener +=1
+    if opener == 1:
+        port = 8080
+        url = "http://127.0.0.1:"+str(port)
+        print("~~~~~~~~~~~~~~~~~~~~~~~Openning~~~~~~~~~~~~~~~~~")
+        threading.Timer(1.25, lambda: webbrowser.open(url)).start()
+    else :
+        opener = 0
 
 def processTrack():
     return
@@ -59,6 +64,18 @@ def MediaPlayer():
                 return render_template("pause.html")
             if click == 0:
                 return render_template("play.html")
+        if "upBtn" in request.form:
+            print("Vol Up")
+            try:
+                os.system("amixer set Master 2%+")
+            except Exception as e:
+                print(str(e))
+        if "downBtn" in request.form:
+            print("Vol Up")
+            try:
+                os.system("amixer set Master 2%-")
+            except Exception as e:
+                print(str(e))
 
 
 if __name__ == '__main__':
