9c28e9bd9ac8f79c2dc5ffd20261c36d33908e26
Adrian <adriansandovalvargas@gmail.com>
2019-09-12 13:31:58 -0400

    Track Stuff

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -3,19 +3,29 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 #sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E/player2 org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track
 click = 0
+song = "-------"
+artist = "-------"
 
 def openWeb():
     port = 8080
     url = "http://127.0.0.1:"+str(port)
     print("~~~~~~~~~~~~~~~~~~~~~~~Openning~~~~~~~~~~~~~~~~~")
     threading.Timer(1.25, lambda: webbrowser.open(url)).start()
-
+    
+def track():
+    cmd = "sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.freedesktop.DBus.Properties.Get string:org.bluez.MediaControl1 string:Player"
+    player = getName(subprocess.check_output(cmd, shell=True))
+    cmd2 = "sudo dbus-send --system --print-reply --dest=org.bluez "+ player +" org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track"                  
+    vals = subprocess.check_output(cmd2,shell=True)
+    processTrack(vals)
+ 
 def processTrack(val):
+    global song, artist
     count = 0 
     first = 0
     last = 0
-    track = 3
-    artist = 19
+    track_index = 3
+    artist_index = 19
     for e in val:
         count += 1
         if( e == "["):
@@ -24,8 +34,8 @@ def processTrack(val):
             last = count
     arr = val[first:last-1]
     li = list(arr.split("\n"))
-    print(getName(li[track]))
-    print(getName(li[artist]))
+    song = getName(li[track_index])
+    artist = getName(li[artist_index])
 
     
 def getName(s):
@@ -39,14 +49,14 @@ def getName(s):
                 last = count
             else:
                 first = count
-    return s[first:last-1]
-                
+    return s[first:last-1]               
     
 @app.route('/', methods=['GET', 'POST'])
 def MediaPlayer():
     global click
     if request.method == 'GET':
-        return render_template("play.html")
+        print("GET")
+        return render_template("play.html", song = song, artist= artist)
     else:
         if "prev_btn" in request.form:
             print("Prev Clicked")
@@ -65,15 +75,6 @@ def MediaPlayer():
                 print(click)
                 try:
                    os.system("sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.bluez.MediaControl1.Play")
-                   cmd = "sudo dbus-send --system --print-reply --dest=org.bluez /org/bluez/hci0/dev_D0_D2_B0_0D_1A_8E org.freedesktop.DBus.Properties.Get string:org.bluez.MediaControl1 string:Player"
-                   player = getName(subprocess.check_output(cmd, shell=True))
-                   cmd2 = "sudo dbus-send --system --print-reply --dest=org.bluez "+player+" org.freedesktop.DBus.Properties.Get string:org.bluez.MediaPlayer1 string:Track"                  
-                   vals = subprocess.check_output(cmd2,shell=True)
-                   processTrack(vals)
-                   #player = player[:-1]
-                   #test = os.popen(cmd).read()
-                   #print("~~~~~~~~~~~~~~~OPEN:\n" + str(vals))
-                   #print("\n~~~~~~~~~~SUB:::" + str(other))
                 except Exception as e:
                     print(str(e))
                 return render_template("pause.html")
@@ -115,12 +116,14 @@ def MediaPlayer():
             except Exception as e:
                 print(str(e))
             if click == 1:
-                return render_template("pause.html")
+                return render_template("pause.html", song = "HI", artist="MIKE")
             if click == 0:
-                return render_template("play.html")
-
+                return render_template("play.html", song = "HI2", artist="MIKE2")
 
 if __name__ == '__main__':
     openWeb()
+    #app.run(debug = True, use_reloader=False, port=8080)
     app.run(debug = True, port=8080)
 
+
+    
